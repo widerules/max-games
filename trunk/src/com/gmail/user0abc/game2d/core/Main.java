918747fb849cc6eb55a859fc0bdaeaa8c4d5e984
user0abc@gmail.com <user0abc@gmail.com@62af6b15-69ef-3cbc-dbc6-a5413eafa569>
2014-10-24 13:14:17 +0000

    start

---
+++ trunk/src/com/gmail/user0abc/game2d/core/Main.java
@@ -0,0 +1,10 @@
+package com.gmail.user0abc.game2d.core;
+
+/**
+ * Created by sergii.ivanov on 10/23/2014.
+ */
+public class Main {
+    public static void Main(String[] args){
+        MainWindow window = new MainWindow();
+    }
+}
---
+++ trunk/src/com/gmail/user0abc/game2d/core/MainWindow.java
@@ -0,0 +1,11 @@
+package com.gmail.user0abc.game2d.core;
+
+import javax.swing.*;
+
+/**
+ * Created by sergii.ivanov on 10/23/2014.
+ */
+public class MainWindow extends JFrame {
+    InfoWindow infoWindow;
+    TilesWindow tilesWindow;
+}
